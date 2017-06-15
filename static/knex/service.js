e527fb894629e3581cbf321c04f9f8e75228b184
Arif <arifmr91@gmail.com>
2017-06-15 10:00:35 +0000

    changes in index.html

--- static/knex/service.js
+++ static/knex/service.js
@@ -12,7 +12,7 @@ angular.module('myApp')
 
 
             
-      			$http.get('http://127.0.0.1:5000/')
+      			$http.get('http://54.254.133.3:5000')
                .success(function (response) {
                    // console.log(response)
                    callback(response);
